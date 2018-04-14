b0733a974eb9ed93615e6a366715d1b142249d62
Rohan Joshi <joshi.rohan09@gmail.com>
2018-04-13 20:08:47 -0400

    third commit. scrolling fixed in navigate.js

--- assets/scripts/navigate.js
+++ assets/scripts/navigate.js
@@ -1,14 +1,14 @@
-'use strict';
-(function () {
+$(document).ready(function () {
 
-  (function () {
+  $(document).ready(function () {
     // bootstrap 4 smooth scrollspy
     $('.nav-item a').on('click', function (event) {
       if (this.hash !== "") {
         event.preventDefault();
         var hash = this.hash;
-        ('html, body').animate({
-          scrollTop: (hash).offset().top
+
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
         }, 400, function () {
           window.location.hash = hash;
         });
@@ -17,7 +17,7 @@
 
     // back to top button
     $('#back-to-top').on('click',function () {
-      ('body,html').animate({
+      $('body,html').animate({
         scrollTop: 0
       }, 400);
       return false;
